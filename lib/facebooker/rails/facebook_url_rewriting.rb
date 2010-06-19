Flameeyes/facebooker

commit df80a7a511aafc78c38e3428556f70a4f3bb2d31

`@request.request_parameters` blows up if there is no env["rack.input"] as of rack-1.1.0. I'm assuming that since request_parameters is an alias (in actionpack) for rack's POST, we can safely assume that link_to_canvas? is false if we're not handling a POST request. This solves the blowing up problem when cucumber step definitions try to generate URLs (Rails 2.3.8 + Rack 1.0.0 + Facebooker + Cucumber + Capybara).

lib/facebooker/rails/facebook_url_rewriting.rb
@@ -44,7 +44,7 @@ module ::ActionController
   
     def rewrite_url_with_facebooker(*args)
       options = args.first.is_a?(Hash) ? args.first : args.last
-      is_link_to_canvas = link_to_canvas?(@request.request_parameters, options)
+      is_link_to_canvas = @request.env["REQUEST_METHOD"] == "POST" && link_to_canvas?(@request.request_parameters, options)
       if is_link_to_canvas && !options.has_key?(:host)
         options[:host] = Facebooker.canvas_server_base
       end 

commit 96b1db2a1922360655eb10bde60c9253ac540183

Fix tests after df80a7a511aafc78c38e3428556f70a4f3bb2d31 broke them.
Instead of assuming that is_link_to_canvas is false (which is not the case
for many of the tests) simply because it's not a POST-request (as Rack
1.1.0 does not allow @request.request_parameters for non-POST requests),
fix the code properly by dropping the request_parameters call.

The params argument of link_to_canvas? was indeed not used at all, so
simply drop it out and pass only the options hash to the function.

lib/facebooker/rails/facebook_url_rewriting.rb
@@ -31,7 +31,7 @@ module ::ActionController
       one_or_true @request.parameters["fb_sig_in_new_facebook"]
     end
 
-    def link_to_canvas?(params, options)
+    def link_to_canvas?(options)
       option_override = options[:canvas]
       return false if option_override == false # important to check for false. nil should use default behavior
       option_override || (can_safely_access_request_parameters? && (one_or_true(@request.parameters["fb_sig_in_canvas"]) || one_or_true(@request.parameters[:fb_sig_in_canvas]) || one_or_true(@request.parameters["fb_sig_is_ajax"]) ))
@@ -44,7 +44,7 @@ module ::ActionController
   
     def rewrite_url_with_facebooker(*args)
       options = args.first.is_a?(Hash) ? args.first : args.last
-      is_link_to_canvas = @request.env["REQUEST_METHOD"] == "POST" && link_to_canvas?(@request.request_parameters, options)
+      is_link_to_canvas = link_to_canvas?(options)
       if is_link_to_canvas && !options.has_key?(:host)
         options[:host] = Facebooker.canvas_server_base
       end 